788658e9d664a23a3aaa9272189fa21a6aaded62
André "MG" Wisén <andre.wisen@gmail.com>
2018-09-06 10:45:13 +0200

    Prepare for Docker Build

--- web/helloDocker.py
+++ web/helloDocker.py
@@ -1,19 +1,18 @@
+# Taken from:
+# https://flask-restful.readthedocs.io/en/latest/quickstart.html
+
 from flask import Flask
 from flask_restful import Resource, Api
-import helloJSON
 
 app = Flask(__name__)
 app.debug = True
 api = Api(app)
 
 class HelloWorld(Resource):
-	def get(self, api_key):
-		data = helloJSON.getJSON(api_key)
-		print(data)
+    def get(self):
+        return {'Hello': 'Docker'}
 
-		return {'Hello': 'World'}
-
-api.add_resource(HelloWorld, '/<string:api_key>')
+api.add_resource(HelloWorld, '/')
 
 if __name__ == '__main__':
     app.run()
